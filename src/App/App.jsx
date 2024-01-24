8f5e352b46dbee43f4219917ba9e0326fced9070
JDavid Ram <150093295+jdavidram@users.noreply.github.com>
2024-01-23 20:59:10 -0500

    Home

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
+import { Fragment, useState } from "react";
+import { FaReact, FaChartLine, FaLeaf } from "react-icons/fa6";
+import { FaInstagram, FaThreads, FaLinkedin, FaGithub } from "react-icons/fa6";
+import { ReactComponent as Logo } from "./logo.svg";
 import './App.scss';
 
 function App() {
+  const [skill, setSkill] = useState("environmental engineer");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Fragment>
+      <header>
+        <span>
+          <Logo />
+          <h3>jdavid.ram</h3>
+        </span>
       </header>
-    </div>
+      <aside>
+        <img src="./img/hello.webp" alt="" />
+      </aside>
+      <main>
+        <h1>Hi, I'm <strong>JDAVID RAM</strong></h1>
+        <div id="skill">
+          <ul>
+            <li>
+              <FaReact onMouseOver={() => {
+                setSkill("frontend developer");
+              }} />
+            </li>
+            <li>
+              <FaChartLine onMouseOver={() => {
+                setSkill("data analyst");
+              }} />
+            </li>
+            <li>
+              <FaLeaf onMouseOver={() => {
+                setSkill("environmental engineer");
+              }} />
+            </li>
+          </ul>
+          <h2 className={ skill.split(" ")[0] }>{ skill }</h2>
+        </div>
+        <h3>Based in Medellin, Colombia</h3>
+      </main>
+      <footer>
+        <ul>
+          <li><a href="https://www.instagram.com/jdavid.ram?igsh=bmE0NTFidzdhdmZt&utm_source=qr" target="_blanck">
+            <FaInstagram />
+          </a></li>
+          <li><a href="https://www.threads.net/@jdavid.ram" target="_blanck">
+            <FaThreads />
+          </a></li>
+          <li><a href="https://www.linkedin.com/in/david-ramirez-rodriguez?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app" target="_blanck">
+            <FaLinkedin />
+          </a></li>
+          <li><a href="https://github.com/jdavidram" target="_blanck">
+            <FaGithub />
+          </a></li>
+        </ul>
+      </footer>
+    </Fragment>
   );
 }
 
