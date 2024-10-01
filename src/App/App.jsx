03d38425b3211a710c8161468108b3343d314d5c
JDavid Ram <150093295+jdavidram@users.noreply.github.com>
2024-10-01 08:01:40 -0500

    Set Error

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
+import { Error } from './Error/Error';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HashRouter>
+      <main>
+        <Routes>
+            <Route path='/' element={ <Error /> } />
+        </Routes>
+      </main>
+    </HashRouter>
   );
 }
 
