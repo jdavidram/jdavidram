754eef063f57efbbd9033d3a837241de7d1bf722
JDavid Ramirez <david456ram@gmail.com>
2024-10-12 09:14:41 -0500

    Create Layout

---
+++ src/App/Layout/Layout.jsx
@@ -0,0 +1,59 @@
+import { FaBars, FaInstagram, FaLinkedin, FaGithub, FaThreads } from "react-icons/fa6";
+import { ReactComponent as Logo } from "../logo.svg";
+import "./Layout.scss";
+import { useState } from "react";
+
+function Layout({ children }) {
+    const [left, setLeft] = useState("0px");
+    function showNav(left) {
+        var universe = document.getElementById("root");
+        if (left === "0px") {
+            universe.style.left = "calc(100vw - 50px)";
+            universe.style.borderRadius = "10px";
+            setLeft("calc(100vw - 50px)");
+        } else {
+            universe.style.left = "0px";
+            universe.style.borderRadius = "0px";
+            setLeft("0px");
+        }
+    }
+    return (
+        <>
+        <header>
+            <span className="nav">
+                <FaBars onClick={() => showNav(left)} />
+            </span>
+            <span className="logo">
+                <Logo />
+                <h3>jdavid.ram</h3>
+            </span>
+            <nav>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                </ul>
+            </nav>
+        </header>
+        <footer>
+            <ul>
+                <li><a target="_blanck" href="https://www.instagram.com/jdavid.ram">
+                    <FaInstagram />
+                </a></li>
+                <li><a target="_blanck" href="https://www.linkedin.com/in/david-ramirez-rodriguez">
+                    <FaLinkedin />
+                </a></li>
+                <li><a target="_blanck" href="https://github.com/jdavidram">
+                    <FaGithub />
+                </a></li>
+                <li><a target="_blanck" href="https://www.threads.net/@jdavid.ram">
+                    <FaThreads />
+                </a></li>
+            </ul>
+        </footer>
+        <main>
+            { children }
+        </main>
+        </>
+    );
+}
+
+export { Layout };
