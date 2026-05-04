7ce4377c1cbc4c120e0d6ebf12c4a060992a5faf
JDavid Ram <150093295+jdavidram@users.noreply.github.com>
2026-05-03 19:30:36 -0500

    Start home

--- src/App/Home/Home.jsx
+++ src/App/Home/Home.jsx
@@ -1,10 +1,13 @@
 import "./Home.scss";
 import { Layout } from "../Layout/Layout";
+import hello from "../img/hello.webp";
 
 function Home() {
     return (
         <Layout>
-            <h1>Hello world!!!</h1>
+            <figure>
+                <img src={ hello } alt="Hello" />
+            </figure>
         </Layout>
     );
 }
--- src/App/Layout/Layout.jsx
+++ src/App/Layout/Layout.jsx
@@ -1,13 +1,29 @@
 import { FaSquareInstagram, FaLinkedin, FaSquareGithub, FaSquareThreads } from "react-icons/fa6";
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import { GiMonsteraLeaf } from "react-icons/gi";
 import { ReactComponent as Logo } from "../logo.svg";
 import "./Layout.scss";
 
 function Layout({ children }) {
+    const moveNav = () => {
+        let universe = document.getElementById('universe');
+        let arrow = document.getElementById('arrow');
+        if (universe.style.left != 'calc(-50px + 100vw)') {
+            universe.style.left = 'calc(-50px + 100vw)';
+            arrow.style.transform = 'rotate(-180deg)';
+        } else {
+            universe.style.left = '0px';
+            arrow.style.transform = 'rotate(0deg)';
+        }
+    };
     return (
-        <main>
+        <main id="universe">
             <header>
                 <span>
-                    <Logo />
+                    <FaRegArrowAltCircleRight id="arrow" onClick={ () => moveNav() } />
+                </span>
+                <span>
+                    <GiMonsteraLeaf id="logo" />
                     <p>jdavid.ram</p>
                 </span>
             </header>
